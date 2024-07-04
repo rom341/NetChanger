35d87ea1a2e55887567ab80c73ae34b8322877b7
rom341 <roman.murzik.antpnov@gmail.com>
2024-07-04 22:05:47 +0400

    reworked for .net 6; added default configuration values

--- NetChanger/NetChanger/Program.cs
+++ NetChanger/NetChanger/Program.cs
@@ -17,6 +17,9 @@ namespace NetChanger
             string pingAddress = config["PingAddress"];
             int checkInterval = int.Parse(config["CheckInterval"]);
 
+            Console.WriteLine($"Ping address: {pingAddress}");
+            Console.WriteLine($"Check interval: {checkInterval}ms");
+
             var networkOperator = new NetworkConnectionOperator(pingAddress, new NetworkService());
 
             while (true)
@@ -35,12 +38,26 @@ namespace NetChanger
                 Thread.Sleep(checkInterval);
             }
         }
-
         static IConfiguration LoadConfiguration()
         {
             var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                .SetBasePath(Directory.GetCurrentDirectory());
+
+            string configFile = Path.Combine("Data", "appsettings.json");
+            if (File.Exists(configFile))
+            {
+                builder.AddJsonFile(configFile, optional: false, reloadOnChange: true);
+            }
+            else
+            {
+                Console.WriteLine("Configuration file not found. Using default values.");
+                var defaultSettings = new Dictionary<string, string>
+                {
+                    { "PingAddress", "8.8.8.8" },
+                    { "CheckInterval", "3000" }
+                };
+                builder.AddInMemoryCollection(defaultSettings);
+            }
 
             return builder.Build();
         }
