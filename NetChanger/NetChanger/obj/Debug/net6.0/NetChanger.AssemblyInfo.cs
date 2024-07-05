fd377559a71366f8b54d345dd3041590ff4b1ddd
rom341 <roman.murzik.antpnov@gmail.com>
2024-07-05 21:06:19 +0400

    reworked for .NET 8; Linux deployment

--- NetChanger/NetChanger/obj/Debug/net6.0/NetChanger.AssemblyInfo.cs
+++ NetChanger/NetChanger/obj/Debug/net6.0/NetChanger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetChanger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a329e0c39244f6f694770769130e8ee5f4233037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d87ea1a2e55887567ab80c73ae34b8322877b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetChanger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetChanger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
